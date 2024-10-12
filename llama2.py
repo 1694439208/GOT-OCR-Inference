d1e06ecdb75ab210443239e0a29e9a44cd580fd0
1694439208 <1694439208@qq.com>
2024-10-12 22:35:46 +0800

    可用demo

--- llama2.py
+++ llama2.py
@@ -2,6 +2,7 @@ import ctypes
 from llama_cpp import Llama
 import llama_cpp
 import llama_cpp.llava_cpp as llava_cpp
+import llama_cpp.llava_cpp
 import torch
 
 def float_list_to_ctypes_array(float_list):
@@ -17,32 +18,43 @@ llm = Llama(
       model_path=r"D:\csx_demo\GOT-OCR2.0\GOT-OCR-2.0-master\GOT_weights\None-464M-123-F16.gguf",
       # n_gpu_layers=-1, # Uncomment to use GPU acceleration
       # seed=1337, # Uncomment to set a specific seed
-      # n_ctx=2048, # Uncomment to increase the context window
+      n_ctx=2048, # Uncomment to increase the context window
+      n_batch=1024
 )
 
 n_past = ctypes.c_int(llm.n_tokens)
 n_past_p = ctypes.pointer(n_past)
 
-tensor = torch.load('tensor.pt')
+tensor = torch.load('tensora.pt')
+print(tensor.shape)
+
 # 将张量转换为 NumPy 数组
-embd_image = tensor.squeeze().cpu().tolist()#.to(torch.float32)
+embd_image = tensor.squeeze().reshape(-1).cpu().tolist()#.to(torch.float32)
 c_float_array = float_list_to_ctypes_array(embd_image)
-embed = llava_cpp.llava_image_embed(embed=c_float_array,n_image_pos=1)
+embed = llava_cpp.llava_image_embed(embed=c_float_array,n_image_pos=286)
 
 with llama_cpp.suppress_stdout_stderr(disable=True):
-    llava_cpp.llava_eval_image_embed(
+    aa = llava_cpp.llava_eval_image_embed(
         llm.ctx,
         embed,
         llm.n_batch,
         n_past_p,
     )
+    print(f"aa:{aa},n_past_p:{n_past.value}")
+# Required to avoid issues with hf tokenizer
+llm.input_ids[llm.n_tokens : n_past.value] = -1
+llm.n_tokens = n_past.value
+# Get prompt tokens to avoid a cache miss
+prompt = llm.input_ids[: llm.n_tokens].tolist()
 
-output = llm(
-      """<|im_start|>system
-You should follow the instructions carefully and explain your answers in detail.<|im_end|><|im_start|>user
-OCR: <|im_end|><|im_start|>assistant""", # Prompt
+
+for token in llm.generate(prompt):
+    print(llm.detokenize([token]).decode(errors="ignore"),end="")
+
+"""output = llm(
+      prompt, # Prompt
       max_tokens=3032, # Generate up to 32 tokens, set to None to generate up to the end of the context window
       stop=["Q:", "\n"], # Stop generating just before the model would generate a new question
       echo=True # Echo the prompt back in the output
 ) # Generate a completion, can also call create_completion
-print(output["choices"][0]["text"])
+print(output["choices"][0]["text"])"""
